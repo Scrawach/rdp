3c3b4ed6a87ebca880b1199d34ff45b08a381550
scrawach <scrawach@gmail.com>
2023-03-22 12:40:27 +0300

    construct example for simplify points in chart

---
+++ example/Example.gd
@@ -0,0 +1,19 @@
+extends Control
+class_name Example
+
+@onready var plot: Plot = get_node("Example/Background Panel/Foreground Panel/Plot")
+@onready var distance_field: Label = get_node("Input Panel/Distance Label")
+
+var previous_distance: float
+
+func _ready() -> void:
+	plot.generate()
+
+func _on_distance_slider_value_changed(distance: float) -> void:
+	plot.simplify(distance)
+	previous_distance = distance
+	distance_field.text = "Distance: %s" % [distance]
+
+func _on_generate_button_pressed() -> void:
+	plot.generate()
+	plot.simplify(previous_distance)
