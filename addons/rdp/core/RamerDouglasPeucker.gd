e726a4ddb8809d7f72a26e642752460e1f4dd97d
Water Zheng <zhengxiaoyao0716@outlook.com>
2024-05-04 17:13:48 +0900

    avoid array copying and slicing
    avoid array copying and slicing

--- addons/rdp/core/RamerDouglasPeucker.gd
+++ addons/rdp/core/RamerDouglasPeucker.gd
@@ -5,12 +5,15 @@ class_name RamerDouglasPeucker
 # Ramer-Douglas-Peucker algorithm: https://en.wikipedia.org/wiki/Ramer%E2%80%93Douglas%E2%80%93Peucker_algorithm
 static func calculate(points: PackedVector2Array, epsilon: float) -> PackedVector2Array:
 	if points.size() < 3:
-		return points
+		return points.duplicate()
 	var epsilon_squared = pow(epsilon, 2)
-	return _simplify(points, 0, points.size() - 1, epsilon_squared)
+	var result := PackedVector2Array()
+	_simplify(result, points, 0, points.size() - 1, epsilon_squared)
+	result.append(points[-1])
+	return result
 
 # Recursive calculation
-static func _simplify(points: PackedVector2Array, start: int, end: int, epsilon_squared: float) -> PackedVector2Array:
+static func _simplify(result: PackedVector2Array, points: PackedVector2Array, start: int, end: int, epsilon_squared: float) -> void:
 	var max_distance_squared = 0
 	var index = 0
 	
@@ -21,15 +24,13 @@ static func _simplify(points: PackedVector2Array, start: int, end: int, epsilon_
 			index = i
 	
 	if max_distance_squared <= epsilon_squared:
-		return PackedVector2Array([points[start], points[end]])
-	
-	var result_1 = _simplify(points, start, index, epsilon_squared)
-	var result_2 = _simplify(points, index, end, epsilon_squared)
-	return result_1.slice(0, result_1.size() - 1) + result_2
+		result.append(points[start])
+	else:
+		_simplify(result, points, start, index, epsilon_squared)
+		_simplify(result, points, index, end, epsilon_squared)
 
 static func _perpendicular_squared(target: Vector2, p1: Vector2, p2: Vector2) -> float:
 	var to_target = target - p1
 	var to_end = p2 - p1
 	var project = to_target.project(to_end)
-	var distance = to_target.length_squared() - project.length_squared()
-	return distance
+	return to_target.length_squared() - project.length_squared()
